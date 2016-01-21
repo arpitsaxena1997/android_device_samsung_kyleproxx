a9fc8443a4542e9272d51e3cdb2f1d9de3e218e8
ishantvivek <ishantvivek1311@gmail.com>
2016-01-21 11:00:50 +0530

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,16 +1,10 @@
 # Release name
-PRODUCT_RELEASE_NAME := GT-S7580
+PRODUCT_RELEASE_NAME := GT-S7582
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/kylepro/device_kylepro.mk)
+$(call inherit-product, device/samsung/kyleprods/device_kyleprods.mk)
 
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := kylepro
-PRODUCT_NAME := cm_kylepro
-PRODUCT_BRAND := samsung
-PRODUCT_MANUFACTURER := samsung
-PRODUCT_MODEL := GT-S7580
-PRODUCT_CHARACTERISTICS := phone
+##
