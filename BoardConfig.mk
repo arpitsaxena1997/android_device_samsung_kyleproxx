31e98f635e2241b55d64bb950db9f2505491b1ef
ishantvivek <ishantvivek1311@gmail.com>
2016-01-30 00:34:56 +0530

    Kylepro>>>Kyleprods

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,7 +51,7 @@ BOARD_FLASH_BLOCK_SIZE                      := 262144
 BOARD_HAVE_BLUETOOTH                        := true
 BOARD_HAVE_BLUETOOTH_BCM                    := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/kyleprods/bluetooth
-BOARD_BLUEDROID_VENDOR_CONF                 := device/samsung/kylepro/bluetooth/libbt_vndcfg_s7582.txt
+BOARD_BLUEDROID_VENDOR_CONF                 := device/samsung/kyleprods/bluetooth/libbt_vndcfg_s7582.txt
 
 # Connectivity - Wi-Fi
 BOARD_HAVE_SAMSUNG_WIFI                     := true
@@ -120,7 +120,7 @@ CHARGING_ENABLED_PATH                       := "/sys/class/power_supply/battery/
 BACKLIGHT_PATH                              := "/sys/class/backlight/panel/brightness"
 
 # healthd
-BOARD_HAL_STATIC_LIBRARIES                  := libhealthd-kylepro.hawaii
+BOARD_HAL_STATIC_LIBRARIES                  := libhealthd-kyleprods.hawaii
 
 # RIL
 BOARD_RIL_CLASS                             := ../../../device/samsung/kyleprods/ril/
